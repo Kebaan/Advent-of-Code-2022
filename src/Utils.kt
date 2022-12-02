7e93a49f02c36e1246426f12e6e682ffb3dc15a0
Kebaan <kebaan@gmail.com>
2022-12-02 14:02:43 +0100

    solved day 1

--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,19 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+
+fun <T> List<T>.splitBy(predicate: (T) -> Boolean): List<List<T>> = this
+    .splitIndices(predicate)
+    .windowed(size = 2, step = 2) { (from, to) ->
+        this.slice(from..to)
+    }
+
+private fun <T> List<T>.splitIndices(predicate: (T) -> Boolean): List<Int> = this
+    .flatMapIndexed { index, x ->
+        when {
+            index == 0 || index == this.lastIndex -> listOf(index)
+            predicate(x) -> listOf(index - 1, index + 1)
+            else -> emptyList()
+        }
+    }
